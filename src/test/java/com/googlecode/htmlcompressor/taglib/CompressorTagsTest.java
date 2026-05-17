bb6b9a910da51e92e9e5ba9ab4ff35b2ab1db2ba
Jeremy Landis <jeremylandis@hotmail.com>
2026-05-17 14:43:26 -0400

    Move test support back to javax namespace after merge from master

--- src/test/java/com/googlecode/htmlcompressor/taglib/CompressorTagsTest.java
+++ src/test/java/com/googlecode/htmlcompressor/taglib/CompressorTagsTest.java
@@ -20,24 +20,26 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import jakarta.el.ELContext;
-import jakarta.servlet.Servlet;
-import jakarta.servlet.ServletConfig;
-import jakarta.servlet.ServletContext;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
-import jakarta.servlet.http.HttpSession;
-import jakarta.servlet.jsp.JspWriter;
-import jakarta.servlet.jsp.PageContext;
-import jakarta.servlet.jsp.tagext.BodyContent;
-
 import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
 import java.io.Writer;
 import java.util.Collections;
 import java.util.Enumeration;
+
+import javax.el.ELContext;
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.PageContext;
+import javax.servlet.jsp.el.ExpressionEvaluator;
+import javax.servlet.jsp.el.VariableResolver;
+import javax.servlet.jsp.tagext.BodyContent;
 
 import org.junit.jupiter.api.Test;
 
@@ -736,6 +738,16 @@ class CompressorTagsTest {
         }
 
         // JspContext abstract methods
+        @Override
+        public VariableResolver getVariableResolver() {
+            return null;
+        }
+
+        @Override
+        public ExpressionEvaluator getExpressionEvaluator() {
+            return null;
+        }
+
         @Override
         public void setAttribute(String name, Object value) {
         }
